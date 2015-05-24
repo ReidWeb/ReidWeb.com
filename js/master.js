0b6bd76c8a57d6396ec32b934ce18743de305200
peter@reidweb.com <peter@alderaan.reidweb.com>
2015-05-24 23:45:04 +0100

    modifed url check to work with new url scheme

--- js/master.js
+++ js/master.js
@@ -1,6 +1,7 @@
 function setSelectedPage(){
 
     var currentPage = location.pathname;
+    prompt(currentPage);
     switch (currentPage){
         case "/":
             document.getElementById("homeLink").setAttribute("class","active");
@@ -10,7 +11,7 @@ function setSelectedPage(){
             document.getElementById("homeLink").setAttribute("class","active");
             document.getElementById("homeLinkMob").setAttribute("class","active");
             break;
-        case "/about.php":
+        case "/about/":
             document.getElementById("aboutLink").setAttribute("class","active");
             document.getElementById("aboutLinkMob").setAttribute("class","active");
             break;
