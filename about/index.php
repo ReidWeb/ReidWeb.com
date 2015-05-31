eebaa26af1a33bba75230b4981a8711ebfd02291
Peter Reid <peter@reidweb.com>
2015-05-31 18:14:44 +0100

    Updated java popout to include image

--- about/index.php
+++ about/index.php
@@ -1,61 +1,61 @@
 <?php
-  //Buffer larger content areas like the main page content
-  ob_start();
+//Buffer larger content areas like the main page content
+ob_start();
 ?>
 <div class="parallax-container">
-        <div class="parallax"><img src="/img/parallax-san-fran-city.jpg"></div>
+    <div class="parallax"><img src="/img/parallax-san-fran-city.jpg"></div>
     <div class="container" id="header-container">
-      <h1 class="header left white-text" id="title">About Me</h1>
+        <h1 class="header left white-text" id="title">About Me</h1>
 
     </div>
-    </div>
+</div>
 
-    <div class="section white z-depth-2" id="who-am-i-container">
+<div class="section white z-depth-2" id="who-am-i-container">
     <div class="row container">
-            <a class="btn-floating btn-large waves-effect waves-light  light-blue lighten-1 smoothScroll z-depth-2" id="who-am-i-jumper" href="#title">
-        <i class="mdi-hardware-keyboard-arrow-down white-text"></i>
-      </a>
+        <a class="btn-floating btn-large waves-effect waves-light  light-blue lighten-1 smoothScroll z-depth-2" id="who-am-i-jumper" href="#title">
+            <i class="mdi-hardware-keyboard-arrow-down white-text"></i>
+        </a>
 
-      <div class="col s12 m4 l3">
-        <img class="responsive-img circle z-depth-2" src="/img/avatar.jpg" alt="peter reid" id="who-am-i-image"/>
+        <div class="col s12 m4 l3">
+            <img class="responsive-img circle z-depth-2" src="/img/avatar.jpg" alt="peter reid" id="who-am-i-image"/>
 
-                  </div>
-      <div class="col s12 m8 l9">
-        <!-- Note that "m8 l9" was added -->
-        <h1 class="light grey-text text-darken-4 header">Who am I?</h1>
-        <p class="light grey-text text-darken-4 paragraph">
-          Hi, my name is Peter Reid and I am a young programmer from just outside Belfast, Northern Ireland.<br/><br/>I have a passion for all things computing related; with a particular focus on computer programming. I have programming experience in a variety of different languages - you can find out more about my programming experience below.<br/><br/>My particular focus at present in software development is on the 'Web' and 'Mobile'; with the intention of improving my skills in these areas by developing applications/sites.<br/><br/>
-          I am currently studying a Foundation Degree in Software Engineering at Belfast Metropolitan College in conjunction with the University of Ulster. I hope to progress to study Software Engineering at university beginning 2016.
+        </div>
+        <div class="col s12 m8 l9">
+            <!-- Note that "m8 l9" was added -->
+            <h1 class="light grey-text text-darken-4 header">Who am I?</h1>
+            <p class="light grey-text text-darken-4 paragraph">
+                Hi, my name is Peter Reid and I am a young programmer from just outside Belfast, Northern Ireland.<br/><br/>I have a passion for all things computing related; with a particular focus on computer programming. I have programming experience in a variety of different languages - you can find out more about my programming experience below.<br/><br/>My particular focus at present in software development is on the 'Web' and 'Mobile'; with the intention of improving my skills in these areas by developing applications/sites.<br/><br/>
+                I am currently studying a Foundation Degree in Software Engineering at Belfast Metropolitan College in conjunction with the University of Ulster. I hope to progress to study Software Engineering at university beginning 2016.
 
-        </p>
-          <a class="btn-floating btn-large waves-effect waves-light  light-blue lighten-1 smoothScroll z-depth-2" id="programming-jumper" href="#programming-section">
-        <i class="mdi-hardware-keyboard-arrow-down white-text"></i>
-      </a>
-      </div>
+            </p>
+            <a class="btn-floating btn-large waves-effect waves-light  light-blue lighten-1 smoothScroll z-depth-2" id="programming-jumper" href="#programming-section">
+                <i class="mdi-hardware-keyboard-arrow-down white-text"></i>
+            </a>
+        </div>
 
     </div>
 
 </div>
 
-    <div class="parallax-container" style="height:250px;">
-        <div class="parallax"><img src="/img/parallax-belfast-titanic.jpg"></div>
-    </div>
+<div class="parallax-container" style="height:250px;">
+    <div class="parallax"><img src="/img/parallax-belfast-titanic.jpg"></div>
+</div>
 
-  <div class="section white" id="programming-section">
+<div class="section white" id="programming-section">
     <div class="row container">
         <div class="col s12 m8 l9">
             <h2 class="header thin">Programming</h2>
-                <p class="light grey-text text-darken-4 paragraph">
-                        I have experience in a variety of programming languages: (click each language to learn more)
-                </p>
+            <p class="light grey-text text-darken-4 paragraph">
+                I have experience in a variety of programming languages: (click each language to learn more)
+            </p>
 
-                <ul>
-                    <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalJava">• Java</a></li>
-                    <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalCsharp">• C#</a></li>
-                    <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalJS">• JavaScript</a></li>
-                    <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalBash">• Bash</a></li>
-                </ul>
-                <p>In the immediate future I am looking to expand my knowledge of programming languages to include both PHP and Ruby</p>
+            <ul>
+                <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalJava">• Java</a></li>
+                <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalCsharp">• C#</a></li>
+                <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalJS">• JavaScript</a></li>
+                <li><a class="modal-trigger grey-text text-darken-1 language-item" href="#modalBash">• Bash</a></li>
+            </ul>
+            <p>In the immediate future I am looking to expand my knowledge of programming languages to include both PHP and Ruby</p>
 
             <div id="modalJS" class="modal modal-fixed-footer">
                 <div class="modal-content">
@@ -74,8 +74,15 @@
                 </div>
             </div>
 
-            <div id="modalJava" class="modal modal-fixed-footer">
+            <div id="modalJava" class="modal">
+                <div class="parallax-container popout-frame-image-container">
+                    <a class="btn-flat waves-effect waves-light popout-back-btn">
+                        <i class="mdi-navigation-arrow-back white-text"></i>
+                    </a>
+                    <div class="parallax popout-frame-image"><img src="/img/about/javaPic.png"></div>
+                </div>
                 <div class="modal-content">
+
                     <h4>Java</h4>
                     <p>I have a large amount of experience in the use of Java for the development of desktop applications.</p>
                     <p>A large amount of this experience was aqquired during my education, however parts of my knowledge were aqquired through my own research.</p>
@@ -84,9 +91,6 @@
                     <p>My first experiences of object oriented programming originate from my use of Java. I have utilized OOP to develop a number of applications in Java both on a personal basis and on an academic basis.</p>
                     <h5>GUI based application development using Java Swing</h5>
                     <p>I have developed a number of applications using Java Swing to build the GUIs for these applications on both a personal and academic basis. In future I hope to utilise JavaFX in applications to expand my knowledge of GUI development in Java.</p>
-                </div>
-                <div class="modal-footer">
-                    <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
                 </div>
             </div>
 
@@ -119,28 +123,28 @@
 
         </div>
 
-      <div class="col s12 m4 l3">
-        <img class="responsive-img circle z-depth-2" src="/img/programmingSummaryPic.jpg" alt="Programming image" id="programming-image"/>
+        <div class="col s12 m4 l3">
+            <img class="responsive-img circle z-depth-2" src="/img/programmingSummaryPic.jpg" alt="Programming image" id="programming-image"/>
 
 
-      </div>
+        </div>
     </div>
-  </div>
+</div>
 
 <div class="parallax-container" style="height:250px;">
-        <div class="parallax"><img src="/img/parallax-shard.jpg"></div>
-    </div>
+    <div class="parallax"><img src="/img/parallax-shard.jpg"></div>
+</div>
 
 <?php
-  //Assign all Page Specific variables
-  $content = ob_get_contents();
-  ob_end_clean();
-  $pagetitle = "About Me | Peter Reid";
-  $headerContent ="<link href='../css/about.css' type='text/css' rel='stylesheet' media='screen,projection'/>
+//Assign all Page Specific variables
+$content = ob_get_contents();
+ob_end_clean();
+$pagetitle = "About Me | Peter Reid";
+$headerContent ="<link href='../css/about.css' type='text/css' rel='stylesheet' media='screen,projection'/>
   <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
   <script src='../js/smoothscroll.js'></script>
   <script src='../js/about.js'></script>
   ";
-  //Apply the template
-  include("../master.php");
+//Apply the template
+include("../master.php");
 ?>
