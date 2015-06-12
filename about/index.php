7ee1af247863badae094c4c0194772b8e2e8c895
Peter Reid <peter@reidweb.com>
2015-06-12 17:26:42 +0100

    Migrated to 'header' & 'footer' templating
    Moved to templating the header and footer individually rather than 'wrapping' the tempate around the content - this is to ensure better compatability with Wordpress etc.

--- about/index.php
+++ about/index.php
@@ -1,6 +1,11 @@
 <?php
-//Buffer larger content areas like the main page content
-ob_start();
+$pagetitle = "About Me | Peter Reid";
+$headerContent ="<link href='../css/about.css' type='text/css' rel='stylesheet' media='screen,projection'/>
+  <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
+  <script src='../js/smoothscroll.js'></script>
+  <script src='../js/about.js'></script>
+  ";
+include_once("../master/header.php");
 ?>
 <div class="parallax-container">
     <div class="parallax"><img src="/img/parallax-san-fran-city.jpg"></div>
@@ -61,69 +66,69 @@ ob_start();
 
         </div>
         <div id="modalJS" class="modal modal-fixed-footer">
-                <div class="modal-content">
-                    <h4>JavaScript</h4>
-                    <p class="light grey-text text-darken-4 paragraph">I have a large amount of experience in the use of JavaScript for a variety of purposes, including:</p>
-                    <h5>Web form validation</h5>
-                    <p class="light grey-text text-darken-4 paragraph">I have used JavaScript on a number of occasions to validate input for web forms; utilizing RegEx to validate input such as email addresses.</p>
-                    <h5>Mathematical Calculations</h5>
-                    <p class="light grey-text text-darken-4 paragraph">On a number of occasions I have used JavaScript to perform rather complex mathematical operations on sets of numbers that have been input by a user.</p>
-                    <h5>Application Logic</h5>
-                    <p class="light grey-text text-darken-4 paragraph">I have utlilized JavaScript to perform application logic on a number of occasions, using JS to display and hide HTML elements based upon logic implemented in JavaScript.</p>
-                    <p class="light grey-text text-darken-4 paragraph">I have also used a small amount of JQuery to develop this site, a skill which I am looking to expand upon in the future. I am also looking to expand my knowledge to include technologies such as NodeJS and AngularJS.</p>
-                </div>
-                <div class="modal-footer">
-                    <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
-                </div>
+            <div class="modal-content">
+                <h4>JavaScript</h4>
+                <p class="light grey-text text-darken-4 paragraph">I have a large amount of experience in the use of JavaScript for a variety of purposes, including:</p>
+                <h5>Web form validation</h5>
+                <p class="light grey-text text-darken-4 paragraph">I have used JavaScript on a number of occasions to validate input for web forms; utilizing RegEx to validate input such as email addresses.</p>
+                <h5>Mathematical Calculations</h5>
+                <p class="light grey-text text-darken-4 paragraph">On a number of occasions I have used JavaScript to perform rather complex mathematical operations on sets of numbers that have been input by a user.</p>
+                <h5>Application Logic</h5>
+                <p class="light grey-text text-darken-4 paragraph">I have utlilized JavaScript to perform application logic on a number of occasions, using JS to display and hide HTML elements based upon logic implemented in JavaScript.</p>
+                <p class="light grey-text text-darken-4 paragraph">I have also used a small amount of JQuery to develop this site, a skill which I am looking to expand upon in the future. I am also looking to expand my knowledge to include technologies such as NodeJS and AngularJS.</p>
             </div>
-
-            <div id="modalJava" class="modal">
-                <div class="popout-frame-image-container" id="java-image-container">
-                    <a class="btn-flat waves-effect waves-light popout-back-btn modal-close">
-                        <i class="mdi-navigation-arrow-back white-text"></i>
-                    </a>
-                </div>
-                <div class="modal-content">
-
-                    <h4>Java</h4>
-                    <p>I have a large amount of experience in the use of Java for the development of desktop applications.</p>
-                    <p>A large amount of this experience was aqquired during my education, however parts of my knowledge were aqquired through my own research.</p>
-                    <p>In future, I am looking to further develop my Java skills and knowledge by developing Android applications.</p>
-                    <h5>Object Oriented Programming with Java</h5>
-                    <p>My first experiences of object oriented programming originate from my use of Java. I have utilized OOP to develop a number of applications in Java both on a personal basis and on an academic basis.</p>
-                    <h5>GUI based application development using Java Swing</h5>
-                    <p>I have developed a number of applications using Java Swing to build the GUIs for these applications on both a personal and academic basis. In future I hope to utilise JavaFX in applications to expand my knowledge of GUI development in Java.</p>
-                </div>
+            <div class="modal-footer">
+                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
             </div>
+        </div>
 
-            <div id="modalCsharp" class="modal modal-fixed-footer">
-                <div class="popout-frame-image-container" id="c-sharp-image-container">
-                    <a class="btn-flat waves-effect popout-back-btn modal-close">
-                        <i class="mdi-navigation-arrow-back black-text"></i>
-                    </a>
-                </div>
-                <div class="modal-content">
-                    <h4>C#</h4>
-                    <p>My experience of C# stems from my use of C# as a part of ASP.NET websites during my education. In future I hope to expand my knowledge of C# to the use of C# for both 'traditional' Windows desktop applications and Windows 10 'universal' applications.</p>
-                    <h5>Server side scripting</h5>
-                    <p>I have utilised C# along with ASP.NET to a 'eCommerce' site with a basic content management system. I utilised C# to load content into the page dynamically based upon an external data source. I also utilised C# to develop a 'cart' system for the site.</p>
-                </div>
+        <div id="modalJava" class="modal">
+            <div class="popout-frame-image-container" id="java-image-container">
+                <a class="btn-flat waves-effect waves-light popout-back-btn modal-close">
+                    <i class="mdi-navigation-arrow-back white-text"></i>
+                </a>
             </div>
+            <div class="modal-content">
 
-            <div id="modalBash" class="modal modal-fixed-footer">
-                <div class="modal-content">
-                    <h4>Bash</h4>
-                    <p>I have utilised bash to 'automate' a number of systems on both my Linux server and laptop. While Bash may not neccessarily be viewed as a 'programming' language, the automated systems that it is enabled me to develop have been extremely useful.</p>
-                    <h5>Server MOTD Script</h5>
-                    <p>One of my earliest uses of Bash was to develop a dynamically generated 'Message of the Day' script for my Linux server. I wanted a means by which I could easily view the 'health' of the system when logging in over SSH; as such I developed a script that displayed information such as the disk and CPU usage of the system when logging in.</p>
-                    <p>The script is open sourced under the MIT license and can be found on <a href="https://github.com/ReidWeb/linuxmotd" target="_blank">GitHub</a>.</p>
-                    <h5>Build Process Automation</h5>
-                    <p>I have utilized bash scripting to automate processes that are completed when building a project using my Jenkins build server. I have written a number of different scripts to automate tasks such as: generating Javadocs for each branch of a git repository and copy them to a web-accessable directory and automatically 'mirroring' a git repository to another remote server when a build is triggered.</p>
-                </div>
-                <div class="modal-footer">
-                    <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
-                </div>
+                <h4>Java</h4>
+                <p>I have a large amount of experience in the use of Java for the development of desktop applications.</p>
+                <p>A large amount of this experience was aqquired during my education, however parts of my knowledge were aqquired through my own research.</p>
+                <p>In future, I am looking to further develop my Java skills and knowledge by developing Android applications.</p>
+                <h5>Object Oriented Programming with Java</h5>
+                <p>My first experiences of object oriented programming originate from my use of Java. I have utilized OOP to develop a number of applications in Java both on a personal basis and on an academic basis.</p>
+                <h5>GUI based application development using Java Swing</h5>
+                <p>I have developed a number of applications using Java Swing to build the GUIs for these applications on both a personal and academic basis. In future I hope to utilise JavaFX in applications to expand my knowledge of GUI development in Java.</p>
             </div>
+        </div>
+
+        <div id="modalCsharp" class="modal modal-fixed-footer">
+            <div class="popout-frame-image-container" id="c-sharp-image-container">
+                <a class="btn-flat waves-effect popout-back-btn modal-close">
+                    <i class="mdi-navigation-arrow-back black-text"></i>
+                </a>
+            </div>
+            <div class="modal-content">
+                <h4>C#</h4>
+                <p>My experience of C# stems from my use of C# as a part of ASP.NET websites during my education. In future I hope to expand my knowledge of C# to the use of C# for both 'traditional' Windows desktop applications and Windows 10 'universal' applications.</p>
+                <h5>Server side scripting</h5>
+                <p>I have utilised C# along with ASP.NET to a 'eCommerce' site with a basic content management system. I utilised C# to load content into the page dynamically based upon an external data source. I also utilised C# to develop a 'cart' system for the site.</p>
+            </div>
+        </div>
+
+        <div id="modalBash" class="modal modal-fixed-footer">
+            <div class="modal-content">
+                <h4>Bash</h4>
+                <p>I have utilised bash to 'automate' a number of systems on both my Linux server and laptop. While Bash may not neccessarily be viewed as a 'programming' language, the automated systems that it is enabled me to develop have been extremely useful.</p>
+                <h5>Server MOTD Script</h5>
+                <p>One of my earliest uses of Bash was to develop a dynamically generated 'Message of the Day' script for my Linux server. I wanted a means by which I could easily view the 'health' of the system when logging in over SSH; as such I developed a script that displayed information such as the disk and CPU usage of the system when logging in.</p>
+                <p>The script is open sourced under the MIT license and can be found on <a href="https://github.com/ReidWeb/linuxmotd" target="_blank">GitHub</a>.</p>
+                <h5>Build Process Automation</h5>
+                <p>I have utilized bash scripting to automate processes that are completed when building a project using my Jenkins build server. I have written a number of different scripts to automate tasks such as: generating Javadocs for each branch of a git repository and copy them to a web-accessable directory and automatically 'mirroring' a git repository to another remote server when a build is triggered.</p>
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+            </div>
+        </div>
 
         <div class="col s12 m4 l3" style="margin-top:30px;">
             <img class="responsive-img circle z-depth-2" src="/img/programmingSummaryPic.jpg" alt="Programming image" id="programming-image"/>
@@ -135,16 +140,4 @@ ob_start();
     <div class="parallax"><img src="/img/parallax-shard.jpg"></div>
 </div>
 
-<?php
-//Assign all Page Specific variables
-$content = ob_get_contents();
-ob_end_clean();
-$pagetitle = "About Me | Peter Reid";
-$headerContent ="<link href='../css/about.css' type='text/css' rel='stylesheet' media='screen,projection'/>
-  <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
-  <script src='../js/smoothscroll.js'></script>
-  <script src='../js/about.js'></script>
-  ";
-//Apply the template
-include("../master.php");
-?>
+<?php include_once("../master/footer.php"); ?>
---
+++ master/footer.php
@@ -0,0 +1,44 @@
+<footer class="page-footer grey lighten-1">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="grey-text text-darken-3">About Me</h5>
+                <p class="grey-text text-darken-3">I am a Software Engineering student from Belfast, Northern Ireland. I have an immense passion for software development. This website is intended as a 'portfolio' of my work. Feel free to have a look around and contact me if you see fit.</p>
+
+
+            </div>
+            <div class="col l3 s12">
+                <h5 class="grey-text text-darken-3">Settings</h5>
+                <ul>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h5 class="grey-text text-darken-3">Connect</h5>
+                <ul>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-darken-3" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright grey darken-3">
+        <div class="container">
+            © 2015 Peter Reid | Built with <a class=" white-text" href="http://materializecss.com">Materialize</a>
+        </div>
+    </div>
+</footer>
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="/js/materialize.js"></script>
+<script src="/js/init.js"></script>
+
+</body>
+</html>
