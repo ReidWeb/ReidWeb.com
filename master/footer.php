30ecfd7a108c294701eb53077ae01dfb38ae83ef
Peter Reid <peter@reidweb.com>
2015-06-12 19:56:05 +0100

    Added social links to footer

--- master/footer.php
+++ master/footer.php
@@ -19,10 +19,10 @@
             <div class="col l3 s12">
                 <h5 class="grey-text text-darken-3">Connect</h5>
                 <ul>
-                    <li><a class="grey-text text-darken-3" href="#!">Link 1</a></li>
-                    <li><a class="grey-text text-darken-3" href="#!">Link 2</a></li>
-                    <li><a class="grey-text text-darken-3" href="#!">Link 3</a></li>
-                    <li><a class="grey-text text-darken-3" href="#!">Link 4</a></li>
+                    <li><a class="grey-text text-darken-3" href="mailto:contact@reidweb.com"><i class="mdi-content-mail"></i> Email</a></li>
+                    <li><a class="grey-text text-darken-3" href="https://github.com/ReidWeb"><i class="fa fa-github"></i> GitHub</a></li>
+                    <li><a class="grey-text text-darken-3" href="https://twitter.com/peterreid29"><i class="fa fa-twitter"></i> Twitter</a></li>
+                    <li><a class="grey-text text-darken-3" href="https://keybase.io/peterreid"><i class="mdi-communication-vpn-key"></i> KeyBase</a></li>
                 </ul>
             </div>
         </div>
