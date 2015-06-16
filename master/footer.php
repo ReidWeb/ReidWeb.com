70c792c5c14dae3f7fd49b3bc0bfff10e9a71d27
Peter Reid <peter@reidweb.com>
2015-06-16 22:25:49 +0100

    More changes to footer

--- master/footer.php
+++ master/footer.php
@@ -3,19 +3,19 @@
         <div class="container">
             © 2015 Peter Reid | Built with <a class=" white-text" href="http://materializecss.com">Materialize</a>
             <div style="float:right;height:100%;">
-                <a class="white-text" href="https://keybase.io/peterreid"><i class="social-icon mdi-communication-vpn-key" style="font-size:20px;"></i></a>
+                <a class="social-icon-anchor" href="https://keybase.io/peterreid"><i class="social-icon mdi-communication-vpn-key" style="font-size:20px;"></i></a>
             </div>
             <div style="float:right;height:100%;margin-top:19px;">
-                <a class="white-text" href="https://twitter.com/reidweb"><i class="social-icon fa fa-twitter fa-lg"></i></a>
+                <a class="social-icon-anchor" href="https://twitter.com/reidweb"><i class="social-icon fa fa-twitter fa-lg"></i></a>
             </div>
             <div style="float:right;height:100%;margin-top:18px;">
-                <a class="white-text" href="https://uk.linkedin.com/in/reidweb"><i class="social-icon fa fa-linkedin fa-lg"></i></a>
+                <a class="social-icon-anchor" href="https://uk.linkedin.com/in/reidweb"><i class="social-icon fa fa-linkedin fa-lg"></i></a>
             </div>
             <div style="float:right;height:100%;margin-top:18px;">
-                <a class="white-text" href="https://github.com/ReidWeb"><i class="social-icon fa fa-github fa-lg"></i></a>
+                <a class="social-icon-anchor" href="https://github.com/ReidWeb"><i class="social-icon fa fa-github fa-lg"></i></a>
             </div>
             <div style="float:right;height:100%;">
-                <a class="white-text" href="mailto:contact@reidweb.com"><i class="social-icon mdi-communication-email" style="font-size:20px;"></i></a>
+                <a class="social-icon-anchor" href="mailto:contact@reidweb.com"><i class="social-icon mdi-communication-email" style="font-size:20px;"></i></a>
             </div>
         </div>
     </div>
