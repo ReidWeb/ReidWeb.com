deacaebb133da1bdb3ddbeb88c36e251623a38a6
peter@reidweb.com <peter@alderaan.reidweb.com>
2015-06-24 22:04:55 +0100

    tweaks to fix header banner on medium devices
    header banner was not displaying as designed on medium devices

--- about/index.php
+++ about/index.php
@@ -54,7 +54,7 @@
             <div class="row center">
                 <h5 class="header col s12 light blue-grey-text text-darken-4" id="banner-text">I am a programmer focussing on Web and Mobile development, and this is my site. Feel free to have a look around!</h5>
             </div>
-            <div class="row center" style="margin-top:14%;margin-bottom:0px;" id="scroll-down-button-div">
+            <div class="row center" id="banner-button" id="scroll-down-button-div">
                 <a  id="download-button" class="smoothScroll" href="#download-button"><i class="small mdi-hardware-keyboard-arrow-down"></i> Scroll Down <i class="small mdi-hardware-keyboard-arrow-down"></i></a>
             </div>
             <br/>
