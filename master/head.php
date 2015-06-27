a0f35d44a8d6f998b5a3e9e41413cd2fda8f6815
Peter Reid <peter@reidweb.com>
2015-06-27 12:46:27 +0100

    Added web-app properties

--- master/head.php
+++ master/head.php
@@ -5,7 +5,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title><?php echo $pagetitle; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
-    <meta name="theme-color" content="#03a9f4">
 
     <!-- CSS  -->
     <link href="/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
@@ -17,6 +16,20 @@
 
     <!--Social Icon Font -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <!--Web App Properties-->
+    <!-- See documentation: https://developers.google.com/web/fundamentals/device-access/stickyness/ -->
+    <!-- Web App Manifest -->
+    <link rel="manifest" href="/manifest.json">
+    <!-- Style the browser elements -->
+    <meta name="theme-color" content="#4fc3f7">
+    <meta name="msapplication-navbutton-color" content="#4fc3f7">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#4fc3f7">
+    <!-- Define web-app homescreen title -->
+    <meta name="application-name" content="ReidWeb.com">
+    <meta name="apple-mobile-web-app-title" content="ReidWeb.com">
+    <!-- Hide the status bar on iOS -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
     <!-- Page specific content -->
     <?php echo $headerContent; ?>
