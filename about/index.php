19e21c9c9c5b69ab646fe593b1cea0e19e430b79
Peter Reid <peter@reidweb.com>
2015-06-29 18:23:46 +0100

    'Modulized' navigator content
    I have moved the 'content' of the nav bars into seperate files, so that changes when adding/removing pages only need to be made to two files rather than each and every file

--- about/index.php
+++ about/index.php
@@ -10,29 +10,10 @@ include_once("../master/head.php");
     <div class="section no-pad-bot light-blue lighten-2 z-depth-0" id="index-banner">
         <div class="navbar-fixed" id="fixed-bar">
             <nav style="background: rgba(255, 255, 255, 0.0);" role="navigation" id="desktop-nav-about">
-                <div class="nav-wrapper container">
-                    <a id="logo-container" href="/" class="brand-logo blue-grey-text text-darken-4 thin">Peter Reid</a>
-                    <ul class="right hide-on-med-and-down">
-                        <li id="homeLink"><a href="/" class="blue-grey-text text-darken-4">Home</a></li>
-                        <li id="projectsLink"><a href="#" class="blue-grey-text text-darken-4">Projects</a></li>
-                        <li id="blogLink"><a href="#" class="blue-grey-text text-darken-4">Blog</a></li>
-                        <li id="aboutLink"><a href="/about" class="blue-grey-text text-darken-4">About Me</a></li>
-                        <li id="contactLink"><a href="#" class="blue-grey-text text-darken-4">Contact</a></li>
-                    </ul>
-
-                    <a href="#" data-activates="nav-mobile" class="button-collapse blue-grey-text text-darken-4"><i class="mdi-navigation-menu"></i></a>
-                </div>
-
+                <?php include_once("../master/navContent.php"); ?>
             </nav>
         </div>
-        <ul id="nav-mobile" class="side-nav white z-depth-5">
-            <div id="nav-mobile-header"><img class="responsive-img circle z-depth-1" src="/img/avatar.jpg" alt="peter reid" /><a id="logo-container" href="/" class="brand-logo white-text bold">peter reid</a></div>
-            <li id="homeLinkMob" class="blue-grey-text text-darken-4"><a href="/"><i class="small mdi-action-home left blue-grey-text text-darken-4"></i>Home</a></li>
-            <li id="projectsLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="small mdi-av-web left 1  blue-grey-text text-darken-4"></i>Projects</a></li>
-            <li id="blogLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="small mdi-action-speaker-notes left blue-grey-text text-darken-4"></i>Blog</a></li>
-            <li id="aboutLinkMob" class="blue-grey-text text-darken-4"><a href="/about"><i class="small mdi-social-person left blue-grey-text text-darken-4"></i>About Me</a></li>
-            <li id="contactLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="small mdi-communication-email left blue-grey-text text-darken-4"></i>Contact</a></li>
-        </ul>
+            <?php include_once("../master/navContentMobile.php"); ?>
 
         <!-- End Nav -->
 
--- master/nav.php
+++ master/nav.php
@@ -1,28 +1,11 @@
 <div class="navbar-fixed">
     <nav class="white lighten-1" role="navigation">
-        <div class="nav-wrapper container">
-            <a id="logo-container" href="/" class="brand-logo blue-grey-text text-darken-4 thin">Peter Reid</a>
-            <ul class="right hide-on-med-and-down">
-                <li id="homeLink"><a href="/" class="blue-grey-text text-darken-4">Home</a></li>
-                <li id="projectsLink"><a href="#" class="blue-grey-text text-darken-4">Projects</a></li>
-                <li id="blogLink"><a href="#" class="blue-grey-text text-darken-4">Blog</a></li>
-                <li id="aboutLink"><a href="/about" class="blue-grey-text text-darken-4">About Me</a></li>
-                <li id="contactLink"><a href="#" class="blue-grey-text text-darken-4">Contact</a></li>
-            </ul>
+        <?php include_once("navContent.php"); ?>
 
-
-            <ul id="nav-mobile" class="side-nav white z-depth-5">
-                <div id="nav-mobile-header"><img class="responsive-img circle z-depth-1" src="/img/avatar.jpg" alt="peter reid" /><a id="logo-container" href="/" class="brand-logo white-text bold">peter reid</a></div>
-                <li id="homeLinkMob" class="blue-grey-text text-darken-4"><a href="/"><i class="tiny mdi-action-home left blue-grey-text text-darken-4"></i>Home</a></li>
-                <li id="projectsLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="tiny mdi-av-web left 1  blue-grey-text text-darken-4"></i>Projects</a></li>
-                <li id="blogLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="tiny mdi-action-speaker-notes left blue-grey-text text-darken-4"></i>Blog</a></li>
-                <li id="aboutLinkMob" class="blue-grey-text text-darken-4"><a href="/about"><i class="tiny mdi-social-person left blue-grey-text text-darken-4"></i>About Me</a></li>
-                <li id="contactLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="tiny mdi-communication-email left blue-grey-text text-darken-4"></i>Contact</a></li>
-            </ul>
-            <a href="#" data-activates="nav-mobile" class="button-collapse blue-grey-text text-darken-4"><i class="mdi-navigation-menu"></i></a>
-        </div>
+        <?php include_once("navContentMobile.php"); ?>
     </nav>
 </div>
+
 
 <script>
     setSelectedPage();
