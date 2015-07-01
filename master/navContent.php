ccb26d8b17bc837a8c06b62098e42a1c56153e17
Peter Reid <peter@reidweb.com>
2015-07-01 17:30:41 +0100

    Added initial contact page

--- master/navContent.php
+++ master/navContent.php
@@ -2,7 +2,7 @@
     <a id="logo-container" href="/" class="brand-logo blue-grey-text text-darken-4 thin">Peter Reid</a>
     <ul class="right hide-on-med-and-down">
         <li id="homeLink"><a href="/" class="blue-grey-text text-darken-4">Home</a></li>
-        <li id="contactLink"><a href="#" class="blue-grey-text text-darken-4">Contact</a></li>
+        <li id="contactLink"><a href="/contact" class="blue-grey-text text-darken-4">Contact</a></li>
     </ul>
     <a href="#" data-activates="nav-mobile" class="button-collapse blue-grey-text text-darken-4"><i class="mdi-navigation-menu"></i></a>
 </div>
