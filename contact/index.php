bc7deb028409750adf5818a6de3ecdb2436156a1
Peter Reid <peter@reidweb.com>
2015-07-01 20:45:31 +0100

    Modified contact page

--- contact/index.php
+++ contact/index.php
@@ -17,8 +17,14 @@ include_once("../master/header.php");
         <div class="container" style="padding-top:10px;">
             <p>
                 There are a number of different means by which you can contact me, the first of these is via <a href="mailto:contact@reidweb.com" target="_blank">email.</a>
-                <br/> I can also be contacted via my <a href="https://uk.linkedin.com/in/reidweb" target="_blank">LinkedIn</a> profile or via my <a href="https://twitter.com/ReidWeb" target="_blank">Twitter</a> profile.
-                <br/> If you wish to send me a sensitive email, you can retrieve my PGP public key from my <a href="https://keybase.io/peterreid" target="_blank">KeyBase</a> profile; send the email to one of the email addresses associated with my PGP key.
+            </p>
+            <p>
+                I can also be contacted via my <a href="https://uk.linkedin.com/in/reidweb" target="_blank">LinkedIn</a> profile or via my <a href="https://twitter.com/ReidWeb" target="_blank">Twitter</a> profile.
+            </p>
+            <p>
+
+                If you wish to send me a sensitive email, you can retrieve my PGP public key from my <a href="https://keybase.io/peterreid" target="_blank">KeyBase</a> profile; send the email to one of the email addresses associated with my PGP key.
+
             </p>
 
         </div>
