236e63fa36ca20358e1abe81e290ce8e9bef6cf7
Peter Reid <peter@reidweb.com>
2015-07-01 18:10:23 +0100

    Removed double include of nav

--- master/header.php
+++ master/header.php
@@ -2,5 +2,4 @@
 $base_dir  = $_SERVER['DOCUMENT_ROOT'];
 include_once($base_dir . "/master/head.php");
 include_once($base_dir . "/master/nav.php");
-include_once($base_dir . "/master/nav.php");
 ?>
