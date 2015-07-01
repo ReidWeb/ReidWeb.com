8cff21adec2dddc847423c875c85ac8d9475a474
Peter Reid <peter@reidweb.com>
2015-07-01 16:31:50 +0100

    Links in nav updated to reflect index page changes

--- master/navContentMobile.php
+++ master/navContentMobile.php
@@ -1,8 +1,5 @@
 <ul id="nav-mobile" class="side-nav white z-depth-5">
     <div id="nav-mobile-header"><img class="responsive-img circle z-depth-1" src="/img/avatar.jpg" alt="peter reid" /><a id="logo-container" href="/" class="brand-logo white-text bold">peter reid</a></div>
     <li id="homeLinkMob" class="blue-grey-text text-darken-4"><a href="/"><i class="small mdi-action-home left blue-grey-text text-darken-4"></i>Home</a></li>
-    <li id="projectsLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="small mdi-av-web left 1  blue-grey-text text-darken-4"></i>Projects</a></li>
-    <li id="blogLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="small mdi-action-speaker-notes left blue-grey-text text-darken-4"></i>Blog</a></li>
-    <li id="aboutLinkMob" class="blue-grey-text text-darken-4"><a href="/about"><i class="small mdi-social-person left blue-grey-text text-darken-4"></i>About Me</a></li>
     <li id="contactLinkMob" class="blue-grey-text text-darken-4"><a href="#"><i class="small mdi-communication-email left blue-grey-text text-darken-4"></i>Contact</a></li>
 </ul>
