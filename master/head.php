5f534f4a14e41903ca0d7b268f3062762869d430
Peter Reid <peter@reidweb.com>
2015-07-01 16:41:30 +0100

    Attempt to fix JS error

--- master/head.php
+++ master/head.php
@@ -16,6 +16,7 @@
 
     <!-- JS -->
     <script src="/js/master.js"></script>
+    <script src="/js/materialize.js"></script>
 
     <!--Social Icon Font -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
