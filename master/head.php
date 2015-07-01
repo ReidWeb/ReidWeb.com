d7dd0cf865c015651990bd6cf8576390ceae90df
Peter Reid <peter@reidweb.com>
2015-07-01 16:43:33 +0100

    further attempts to fix serverside hosted js issue

--- master/head.php
+++ master/head.php
@@ -16,6 +16,7 @@
 
     <!-- JS -->
     <script src="/js/master.js"></script>
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
     <script src="/js/materialize.js"></script>
 
     <!--Social Icon Font -->
