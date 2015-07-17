1b9567c3c38dc78a75fff0bdec482aaec0cbf869
Peter Reid <peter@reidweb.com>
2015-07-17 09:38:49 +0100

    Removed build files

--- build/master/header.php
+++
@@ -1,1 +0,0 @@
-<?php $base_dir = $_SERVER['DOCUMENT_ROOT']; include_once($base_dir . "/master/head.php"); include_once($base_dir . "/master/nav.php"); ?>
--- build/master/nav.php
+++
@@ -1,1 +0,0 @@
-<div class="navbar-fixed"><nav class="white lighten-1" role="navigation"><?php include_once("navContent.php"); ?><?php include_once("navContentMobile.php"); ?></nav></div>
