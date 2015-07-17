71d7eac8516a21d0e8e936e1d371742d62d1d135
Peter Reid <peter@reidweb.com>
2015-07-17 19:15:54 +0100

    Changed loading url of Piwik

--- master/analyticstracking.php
+++ master/analyticstracking.php
@@ -15,7 +15,7 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//analytics.reidweb.com/";
+    var u="http://analytics.reidweb.com/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 2]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
