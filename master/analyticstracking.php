77ee64aceeba337b8d8b57d17e21d9bb51ae8ad3
Peter Reid <peter@reidweb.com>
2015-07-17 19:19:12 +0100

    removed piwik tracking

--- master/analyticstracking.php
+++ master/analyticstracking.php
@@ -9,18 +9,3 @@
   ga('send', 'pageview');
 
 </script>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="http://analytics.reidweb.com/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 2]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//analytics.reidweb.com/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
