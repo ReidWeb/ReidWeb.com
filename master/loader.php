6a2e5e59fadb212365c7b4d97f38f2467328eaeb
Peter Reid <peter@reidweb.com>
2015-07-23 15:34:52 +0100

    Moved Jquery loading position

--- master/loader.php
+++ master/loader.php
@@ -1,3 +1,4 @@
+<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
 <script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>
