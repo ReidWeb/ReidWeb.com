89cbccff3fcac026cd06646225d35ad199cae1a8
Peter Reid <peter@reidweb.com>
2015-07-24 21:32:14 +0100

    Migrated to using analtics.js from cdn
    Analytics.js is now cached on google cloud storage and is refreshed every 12 hrs.

--- master/analyticstracking.php
+++ master/analyticstracking.php
@@ -1,9 +1,10 @@
 <!-- Google Analytics -->
+<!-- Note use of cached analytics.js -->
  <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','//storage.googleapis.com/cdn.reidweb.com/analytics.js','ga');
 
   ga('create', 'UA-36748721-1', 'auto');
   ga('send', 'pageview');
