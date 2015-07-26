1d4bf03889dc3b71315abfd66a5784fd10a5813c
peter@reidweb.com <peter@alderaan.reidweb.com>
2015-07-26 23:16:21 +0100

    Added email field to form

--- contact/contactForm.php
+++ contact/contactForm.php
@@ -1,61 +1,68 @@
 <div id="mailFormModal" class="modal">
-  <div class="modal-content">
-    <div class="row" id="email-form">
-      <form class="col s12">
-        <div class="row">
-          <div class="input-field col s12 m6 l6">
-            <i class="material-icons prefix">person</i>
-            <input class="icon_prefix" type="text" class="validate">
-            <label for="icon_prefix">First Name</label>
-          </div>
-          <div class="input-field col s12 m6 l6">
-            <i class="material-icons prefix">person</i>
-            <input class="icon_prefix2" type="text" class="validate">
-            <label for="icon_prefix2">Last Name</label>
-          </div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12 m6 l6">
-            <i class="material-icons prefix">business</i>
-            <input class="icon_prefix4" type="text" class="validate">
-            <label for="icon_prefix4">Organisation</label>
-          </div>
-          <div class="input-field col s12 m6 l6">
-            <i class="material-icons prefix">account_circle</i>
-            <input class="icon_prefix3" type="text" class="validate">
-            <label for="icon_prefix3">Position in organisation</label>
-          </div>
-        </div>
+    <div class="modal-content">
+        <div class="row" id="email-form">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">person</i>
+                        <input class="icon_prefix" type="text" class="validate">
+                        <label for="icon_prefix">First Name</label>
+                    </div>
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">person</i>
+                        <input class="icon_prefix2" type="text" class="validate">
+                        <label for="icon_prefix2">Last Name</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">business</i>
+                        <input class="icon_prefix4" type="text" class="validate">
+                        <label for="icon_prefix4">Organisation</label>
+                    </div>
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">account_circle</i>
+                        <input class="icon_prefix3" type="text" class="validate">
+                        <label for="icon_prefix3">Position in organisation</label>
+                    </div>
+                </div>
 
-        <div class="row">
-          <div class="input-field col s12 m6 l6">
-            <i class="material-icons prefix">subject</i>
-            <input class="icon_prefix5" type="text" class="validate">
-            <label for="icon_prefix5">Subject Line</label>
-          </div>
+                <div class="row">
+                    <div class="input-field col s12 m12 l12">
+                        <i class="material-icons prefix">subject</i>
+                        <input class="icon_prefix5" type="text" class="validate">
+                        <label for="icon_prefix5">Subject Line</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12 m6 l6">
+                        <i class="material-icons prefix">email</i>
+                        <input class="icon_prefix5" type="text" class="validate">
+                        <label for="icon_prefix5">Email Address</label>
+                    </div>
 
-          <div class="input-field col s12 m6 l6">
-              <select>
-                <option class="light-blue-text text-lighten-2" value="" disabled selected>Choose your option</option>
-                <option value="1">Employment Opportunity</option>
-                <option value="2">Software development services</option>
-                <option value="3">Web development services</option>
-                <option value="4">App development services</option>
-                <option value="5">Other</option>
-              </select>
-              <label>Category</label>
+                    <div class="input-field col s12 m6 l6">
+                        <select>
+                            <option class="light-blue-text text-lighten-2" value="" disabled selected>Choose your option</option>
+                            <option value="1">Employment Opportunity</option>
+                            <option value="2">Software development services</option>
+                            <option value="3">Web development services</option>
+                            <option value="4">App development services</option>
+                            <option value="5">Other</option>
+                        </select>
+                        <label>Category</label>
 
-          </div>
-        </div>
-        <div class="row">
-        <div class="input-field col s12">
-          <i class="material-icons prefix">mode_edit</i>
-          <textarea id="icon_prefix6" class="materialize-textarea" type="text"></textarea>
-          <label for="icon_prefix6">Message</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">mode_edit</i>
+                        <textarea id="icon_prefix6" class="materialize-textarea" type="text"></textarea>
+                        <label for="icon_prefix6">Message</label>
+                    </div>
+                </div>
+                <a class="waves-effect waves-light btn light-blue lighten-2" style="float:right;"><i class="material-icons right">send</i>Send</a>
+            </form>
         </div>
     </div>
-    <a class="waves-effect waves-light btn light-blue lighten-2" style="float:right;"><i class="material-icons right">send</i>Send</a>
-    </form>
-  </div>
-</div>
 </div>
