96106d0d1efafe8a14381d19b4334339c02f05b4
Peter Reid <peter@reidweb.com>
2015-07-26 13:11:49 +0100

    Corrected bug in displaying mobile nav on contact page

--- master/nav.php
+++ master/nav.php
@@ -1,8 +1,6 @@
 <div class="navbar-fixed">
     <nav class="white lighten-1" role="navigation">
         <?php include_once("navContent.php"); ?>
-
-        <?php include_once("navContentMobile.php"); ?>
     </nav>
 </div>
-
+<?php include_once("navContentMobile.php"); ?>
