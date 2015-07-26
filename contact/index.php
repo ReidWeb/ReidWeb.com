1b996de210a76986db2d770db1ad4e8321aaba06
Peter Reid <peter@reidweb.com>
2015-07-26 21:56:36 +0100

    Added initial draft of contact form content

--- contact/index.php
+++ contact/index.php
@@ -6,16 +6,22 @@ include_once("../master/header.php");
   <div id="content">
     <div id="contact-banner">
 
-        <div class="container" style="padding-top:10px; z-index:2;">
-          <div class="card large" id="content-card">
-            <div id="content-card-image-container">
-              <div class="glass"><div id="content-card-title">Contact</div></div>
-
-
+      <div class="container" style="padding-top:10px; z-index:2;">
+        <div class="card large" id="content-card">
+          <div id="content-card-image-container">
+            <div class="glass">
+              <div id="content-card-title">Contact</div>
             </div>
-
+            <div id="content-card-content">
+              <h5 class="content-card-header bold">Email</h5>
+              <p>I can be contacted via email using the form below, please select a 'topic' when you complete the form to ensure that your message is addressed correctly.</p>
+              <a class="waves-effect waves-light light-blue lighten-2 btn modal-trigger" style="margin-top:10px;" data-target="mailFormModal"><i class="material-icons left">mail</i>Send me an email</a>
+              <?php include_once("contactForm.php"); ?>
+            </div>
           </div>
+
         </div>
+      </div>
 
     </div>
 
--- js/contact.js
+++ js/contact.js
@@ -12,3 +12,10 @@ $(document).ready(function () {
         setHeight();
     });
 });
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+$(document).ready(function() {
+    $('select').material_select();
+  });
