d103e089fa30d8793b53153015e3491645ed63aa
Peter Reid <peter@reidweb.com>
2015-07-27 20:09:52 +0100

    Async css files

--- master/head.php
+++ master/head.php
@@ -10,11 +10,11 @@
   <link rel='shortcut icon' type='image/x-icon' href='/img/icons/favicon.ico' />
 
   <!-- CSS  -->
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+  <link async rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
   <!--Social Icon Font -->
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link async rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <!-- Master Style -->
-  <link href="https://storage.googleapis.com/cdn.reidweb.com/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link async href="https://storage.googleapis.com/cdn.reidweb.com/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
   <!-- Page specific content -->
   <?php echo $headerContent; ?>
 
